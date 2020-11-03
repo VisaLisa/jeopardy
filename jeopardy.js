2c908b890113f3ad6ce0894a97c27e13cddd63ce
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-11-03 00:45:14 -0800

    finished project

--- jeopardy.js
+++ jeopardy.js
@@ -21,6 +21,8 @@
 let categories = [];
 const NUM_CATEGORIES = 6;
 const NUM_QUESTIONS_PER_CAT = 5;
+const game = $("#game");
+
 
 
 /** Get NUM_CATEGORIES random category from API.
@@ -52,15 +54,20 @@ function getCategoryIds(catIds) {
  *   ]
  */
 
+
+
 function getCategory(catId) {
     const cat = catId.data;
     //show only 5 clues
     const clues = _.sampleSize(catId.data, NUM_QUESTIONS_PER_CAT);
+
+    //Return object with data about a category:
     const catData = {
         title: cat[0].category.title,
         clues: [],
     };
 
+    //Clue array
     clues.map((arr)=>{
         let cluesArr = {
 			question: arr.question,
@@ -69,8 +76,9 @@ function getCategory(catId) {
 		};
 		catData.clues.push(cluesArr);
     });
-    console.log(catData);
-   
+    categories.push(catData);
+    //console.log(categories);
+    
 }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
@@ -81,10 +89,29 @@ function getCategory(catId) {
  *   (initally, just show a "?" where the question/answer would go.)
  */
 
-function fillTable() {
-    const $thead = $('<thead></thead>');
-    const $theadRow = $('<tr></tr>');
-    const $tbody = $('<tbody></tbody>');
+function fillTable(categories) {
+    let titles = categories.map((title) => {
+		return title.title;
+    });
+    
+    //create title/headers
+    $("thead").add("tr");
+	for (let i = 0; i < NUM_CATEGORIES; i++) {
+		const catHeader = document.createElement("th");
+		catHeader.innerText = titles[i];
+		$("thead").append(catHeader);
+    }
+    //create clues
+    for (let y = 0; y < NUM_QUESTIONS_PER_CAT; y++) {
+		const row = document.createElement("tr");
+		for (let x = 0; x < NUM_CATEGORIES; x++) {
+			const box = document.createElement("td");
+			box.innerHTML = `<div id=${x}-${y}>?</div>`;
+			row.append(box);
+		}
+        game.append(row);
+        game.on("click", "td", handleClick);
+	}
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -96,6 +123,29 @@ function fillTable() {
  * */
 
 function handleClick(evt) {
+//   x will show* - if currently null, show question & set .showing to "question"
+//  y will show* - if currently "question", show answer & set .showing to "answer"
+//  z will show* - if currently "answer", ignore click
+const $clickedClue = evt.target;
+const x = $clickedClue.id[0];
+const y = $clickedClue.id[2];
+
+ // if question is clicked, switch to answer
+if ($clickedClue.classList.contains("question")){
+    $clickedClue.innerText = categories[x].clues[y].answer;
+    $clickedClue.classList.remove("question");
+    $clickedClue.classList.add("answer");
+} else  // if null is clicked, switch to question
+{
+    $clickedClue.innerText = categories[x].clues[y].question;
+    $clickedClue.classList.add("question");
+}
+
+// display answer - does not click through
+if ($clickedClue.classList.contains("answer")){
+    return;
+}
+     
 }
 
 /** Wipe the current Jeopardy board, show the loading spinner,
@@ -103,12 +153,17 @@ function handleClick(evt) {
  */
 
 function showLoadingView() {
-
+//remove table and reset categories
+    $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+    $(window).on('load', function(){
+    setTimeout(hideLoadingView, 2000); //wait for page load PLUS two seconds.
+    });
 }
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
 function hideLoadingView() {
+    $('#loadingDiv').remove();
 }
 
 /** Start game:
@@ -119,6 +174,8 @@ function hideLoadingView() {
  * */
 
 async function setupAndStart() {
+    
+    showLoadingView();
     //get 100 categories from API
     const response = await axios.get('http://jservice.io/api/categories',{
         params: {
@@ -143,15 +200,20 @@ async function setupAndStart() {
         //Use catId to get data from clueCard 
         getCategory(clueCard);
     }
-   
-
+    fillTable(categories);
+    hideLoadingView();
 
 }
 
 /** On click of start / restart button, set up game. */
 
-// TODO
+$('#restart').on('click', function(){
+    location.reload();
+    setupAndStart();    
+    event.preventDefault();
+});
 
 /** On page load, add event handler for clicking clues */
-
-// TODO
+$(document).ready(function() {
+	setupAndStart();
+});
